4ccd2df3f23e4c289aa1a728500621bddbe3f1ae
RichiePichie <pokuri0777@gmail.com>
2024-09-04 20:44:53 +0200

    added variable

--- lib/screens/login_register/register.dart
+++ lib/screens/login_register/register.dart
@@ -12,6 +12,7 @@ class _RegisterState extends State<Register> {
   late final TextEditingController _killNiggas;
   late final TextEditingController _password;
   late final TextEditingController _userName;
+  late final TextEditingController _userNae;
   @override
   void initState() {
     _email = TextEditingController();
